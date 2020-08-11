41b5af2ef6cc13a06ac4564f21102d7dab3a15dd
afigurekim <afigurekim@gmail.com>
2020-08-11 18:16:38 +0900

    assets 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopNavBar />
+    <LeftNavBar />
+    <router-view :key="$route.fullPath" />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopNavBar from '@/components/TopNavBar'
+import LeftNavBar from '@/components/LeftNavBar'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopNavBar,
+    LeftNavBar,
+    Footer
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,5 +3,6 @@
 const path = require('path');
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "../src/main/resources/static")
+  outputDir: path.resolve(__dirname, "../src/main/resources/static"),
+  css: { requireModuleExtension: false }
 }
