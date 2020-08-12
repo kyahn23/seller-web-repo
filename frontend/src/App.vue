65f750ec2492ac59569e7e83ae11e68a8de574ae
afigurekim <afigurekim@gmail.com>
2020-08-12 14:46:54 +0900

    frontend update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="app">
     <TopNavBar />
-    <LeftNavBar />
-    <router-view :key="$route.fullPath" />
+
+    <div id="container">
+      <div class="fixedWidth">
+        <LeftNavBar />
+        <!-- <router-view :key="$route.fullPath" /> -->
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
 
 <script>
-import TopNavBar from '@/components/TopNavBar'
-import LeftNavBar from '@/components/LeftNavBar'
-import Footer from '@/components/Footer'
+import TopNavBar from "@/components/TopNavBar";
+import LeftNavBar from "@/components/LeftNavBar";
+import Footer from "@/components/Footer";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     TopNavBar,
     LeftNavBar,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
 
 <style>
---
+++ frontend/src/components/LeftNavBar.vue
@@ -0,0 +1,130 @@
+<template>
+  <!-- left -->
+  <div class="sectionLeft">
+    <div class="lnbBgBox">
+      <ul class="accordion" id="LNB">
+        <li class="depthBox">
+          <a href="#" class="dep1">
+            <span>상담 관리</span>
+          </a>
+          <ul>
+            <li class="current">
+              <a href="service-inbox.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-vendor" alt="아이콘" />
+                <span>상담접수관리</span>
+              </a>
+            </li>
+            <li>
+              <a href="service-visit.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-user" alt="아이콘" />
+                <span>방문개통관리</span>
+              </a>
+            </li>
+            <li>
+              <a href="service-status.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-pw" alt="아이콘" />
+                <span>상담관리현황</span>
+              </a>
+            </li>
+            <li>
+              <a href="service-marketing.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-pw" alt="아이콘" />
+                <span>마케팅대상관리</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="depthBox">
+          <a href="#" class="dep1">
+            <span>SKT 관리</span>
+          </a>
+          <ul>
+            <li>
+              <a href="carr-mntrt-manage.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-list" alt="아이콘" />
+                <span>요금제등록관리</span>
+              </a>
+            </li>
+            <li>
+              <a href="carr-deal-manage.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-list" alt="아이콘" />
+                <span>판매정책관리</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="depthBox">
+          <a href="#" class="dep1">
+            <span>KT 관리</span>
+          </a>
+          <ul>
+            <li>
+              <a href="#">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-list" alt="아이콘" />
+                <span>요금제등록관리</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-list" alt="아이콘" />
+                <span>판매정책관리</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="depthBox">
+          <a href="#" class="dep1">
+            <span>LGU+ 관리</span>
+          </a>
+          <ul>
+            <li>
+              <a href="#">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-list" alt="아이콘" />
+                <span>요금제등록관리</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-list" alt="아이콘" />
+                <span>판매정책관리</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="depthBox">
+          <a href="#" class="dep1">
+            <span>매장 관리</span>
+          </a>
+          <ul>
+            <li>
+              <a href="shop-manage.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-nfc" alt="아이콘" />
+                <span>매장정보관리</span>
+              </a>
+            </li>
+            <li>
+              <a href="shop-employees.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-nfc2" alt="아이콘" />
+                <span>직원관리</span>
+              </a>
+            </li>
+            <li>
+              <a href="shop-notice.html">
+                <img src="../assets/images/cmmn/hidden.png" class="ico-nfc2" alt="아이콘" />
+                <span>공지사항관리</span>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- //left -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
