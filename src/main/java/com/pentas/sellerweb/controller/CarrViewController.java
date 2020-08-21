07dfe52f1d20503f4f79da5dc6e7617bf676ae35
kyahn23 <kyahn23@gmail.com>
2020-08-21 17:43:14 +0900

    v-cloak 적용

--- src/main/java/com/pentas/sellerweb/controller/CarrViewController.java
+++ src/main/java/com/pentas/sellerweb/controller/CarrViewController.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class CarrViewController {
 
     @GetMapping("/carr/deal/{carrier}")
-    public String carrDeal(@PathVariable String carrier, Model model){
-        model.addAttribute("carr", carrier);
+    public String carrDeal(@PathVariable String carrier){
         return "page/carr-deal-manage";
     }
 
     @GetMapping("/carr/mntrt/{carrier}")
-    public String carrMntrt(){
+    public String carrMntrt(@PathVariable String carrier){
         return "page/carr-mntrt-manage";
     }
 }
