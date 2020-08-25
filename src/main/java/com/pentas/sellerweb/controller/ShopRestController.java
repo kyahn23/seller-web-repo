982395ace9dce2816dbde752289b9b17ce829fc7
afigurekim <afigurekim@gmail.com>
2020-08-25 14:17:22 +0900

    VueTheMask 추가

--- src/main/java/com/pentas/sellerweb/controller/ShopRestController.java
+++ src/main/java/com/pentas/sellerweb/controller/ShopRestController.java
@@ -10,12 +10,18 @@ public class ShopRestController {
     @PostMapping("/shop/getShopInfo")
     public DevMap getShopInfo() {
         DevMap rslt = new DevMap();
+        rslt.put("bnNo", "12345678901234567890");
+        rslt.put("bnRegNo", "1234567890");
         rslt.put("bnNm","가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아");
+        rslt.put("ownerNm","가나다라마바사아자차카파타하가나다라마바");
+        rslt.put("bnRegCard","가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아");
         rslt.put("bnAddrSido","가나다라마바사아자차카파");
-        rslt.put("bnAddrSigg","가나다라마바사아자차카파타하");
+        rslt.put("bnAddrSigg","가나다라마바사아자차카파");
         rslt.put("bnAddrDtl", "가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아자차");
-        rslt.put("bnRegNo", "가나다라마바사아자차카파타하가나다라마바");
-        rslt.put("bnImg", "가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아");
+        rslt.put("mstMbrId", "가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아자차카파타하가나다라마바사아");
+        rslt.put("cardChkYn", "Y");
+        rslt.put("bnPnNo", "01234567890");
+        rslt.put("bnImg", "shop.png");
 
         return rslt;
     }
---
+++ src/main/java/com/pentas/sellerweb/service/ShopService.java
@@ -0,0 +1,17 @@
+package com.pentas.sellerweb.service;
+
+import com.pentas.sellerweb.common.dao.CmmnDao;
+import com.pentas.sellerweb.common.module.util.DevMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShopService {
+
+    @Autowired
+    CmmnDao cmmnDao;
+
+    public DevMap shopInfo(DevMap param) {
+        return cmmnDao.selectOne(null, null);
+    }
+}
