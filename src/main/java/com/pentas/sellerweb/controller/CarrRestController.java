810c8a0289217ce39a0bbbc9818b281eba0b7212
kyahn23 <kyahn23@gmail.com>
2020-09-03 11:33:57 +0900

    요금제 등록여부 업체번호 조건 추가 판매정책 현황 리스트 불러오기 추가

--- src/main/java/com/pentas/sellerweb/controller/CarrRestController.java
+++ src/main/java/com/pentas/sellerweb/controller/CarrRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.text.ParseException;
 import java.util.List;
 
 @RestController
@@ -16,6 +16,11 @@ public class CarrRestController {
     @Autowired
     CarrService carrService;
 
+    /**
+     * 요금제 현황 목록 가져오기
+     * @param param
+     * @return
+     */
     @PostMapping("/carr/getMntrtList")
     public DevMap getMntrtList(@RequestBody DevMap param) {
         DevMap rslt = new DevMap();
@@ -26,6 +31,11 @@ public class CarrRestController {
         return rslt;
     }
 
+    /**
+     * 요금제 사용
+     * @param param
+     * @return
+     */
     @PostMapping("/carr/insertUseMnt")
     public DevMap insertUseMnt(@RequestBody List<DevMap> param) {
         DevMap rslt = new DevMap();
@@ -37,12 +47,27 @@ public class CarrRestController {
         return rslt;
     }
 
+    /**
+     * 요금제 미사용처리
+     * @param param
+     * @return
+     */
     @PostMapping("/carr/deleteUseMnt")
     public DevMap deleteUseMnt(@RequestBody List<DevMap> param) {
         DevMap rslt = new DevMap();
         carrService.deleteUseMnt(param);
         rslt.put("succ", "Y");
 
+        return rslt;
+    }
+
+    @PostMapping("/carr/getSellDeviceList")
+    public DevMap getSellDeviceList(@RequestBody DevMap param) {
+        DevMap rslt = new DevMap();
+        List<DevMap> resultList = null;
+
+        resultList = carrService.getSellDeviceList(param);
+        rslt.put("sellDeviceList", resultList);
         return rslt;
     }
 
